1941e1395258b78098169957bfde00ed9dad503a
马嘉远 <944414275@qq.com>
2019-08-29 16:20:47 +0800

    添加项目文件。

---
+++ Content/SettingsAppearance.xaml.cs
@@ -0,0 +1,46 @@
+﻿using FirstFloor.ModernUI.Presentation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+
+namespace FirstFloor.ModernUI.App.Content
+{
+    /// <summary>
+    /// Interaction logic for SettingsAppearance.xaml
+    /// </summary>
+    public partial class SettingsAppearance : UserControl
+    {
+        public SettingsAppearance()
+        {
+            InitializeComponent();
+
+            // a simple view model for appearance configuration
+            this.DataContext = new SettingsAppearanceViewModel();
+            
+            System.Drawing.Text.InstalledFontCollection fonts = new System.Drawing.Text.InstalledFontCollection();
+            int i = 0;
+            foreach (System.Drawing.FontFamily ff in fonts.Families)
+            {
+                if (ff.Name.Equals("宋体"))
+                {
+                    this.fontFamilies.Text = ff.Name;
+                    this.fontFamilies.SelectedIndex = i;
+                }
+                this.fontFamilies.Items.Add(ff.Name);
+                i++;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using FirstFloor.ModernUI.Windows;
+using FirstFloor.ModernUI.Windows.Controls;
+using Hardcodet.Wpf.TaskbarNotification;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FirstFloor.ModernUI.App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ModernWindow
+    {
+        private TaskbarIcon ntfiMain;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.msg.DisplayName = "111111111";
+            CommandBinding cb = new CommandBinding();
+            cb.Command = CustomCommands.CustomCommand;
+            cb.Executed += new ExecutedRoutedEventHandler(OnCloseCommands);
+            this.CommandBindings.Add(cb);
+            this.ntfiMain = new TaskbarIcon();
+            //this.ntfiMain.Icon = Properties.Resources.ccmp;
+            this.ntfiMain.ShowBalloonTip("欢迎", "欢迎使用该系统", BalloonIcon.Info);
+            this.ntfiMain.TrayBalloonTipClicked += new RoutedEventHandler(this.ntfiMain_BalloonTipClicked);
+            this.ntfiMain.TrayLeftMouseDown += new RoutedEventHandler(this.ntfiMain_Click);
+            this.ntfiMain.Visibility = Visibility.Hidden;
+        }
+        private void OnCloseCommands(object sender, ExecutedRoutedEventArgs e)
+        {
+            MessageBox.Show("响应自定义命令MyCommand"); 
+            SystemCommands.CloseWindow(this);
+        }
+        private void ntfiMain_Click(object sender, RoutedEventArgs e)
+        {
+
+            this.ntfiMain.Visibility = Visibility.Hidden;
+            this.Visibility = Visibility.Visible;
+            this.WindowState = System.Windows.WindowState.Normal;
+            this.Show();
+        }
+
+        private void ntfiMain_BalloonTipClicked(object sender, RoutedEventArgs e)
+        {
+            this.ntfiMain.Visibility = Visibility.Hidden;
+            this.Visibility = Visibility.Visible;
+            this.WindowState = System.Windows.WindowState.Normal;
+
+            //this.tabCtrMain.SelectedIndex = 2;
+
+        }
+    }
+}
